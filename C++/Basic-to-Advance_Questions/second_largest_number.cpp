2ab4cf06b1e157a86a309c6294436af774982510
Amanmandal-M <amanmandal644@gmail.com>
2023-09-10 17:21:32 +0530

    second_largest_number

---
+++ C++/Basic-to-Advance_Questions/second_largest_number.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+int find_second_largest(int arr[], int size);
+
+int main(){
+    int arr[7] = {5, 2, 8, 1, 3, 9, 4};
+    
+    int result = find_second_largest(arr, 7);
+    std::cout << "The second largest element is: " << result << std::endl;
+    
+    return 0;
+}
+
+
+int find_second_largest(int arr[], int size) {
+    
+    int max = 0;         
+    int second_max = 0;
+
+    //  0, 1, 2, 3, 4, 5, 6
+    // [5, 2, 8, 1, 3, 9, 4]
+    
+    for(int i=0; i<size; i++){
+        if(arr[i] > max){
+            second_max = max; 
+            max = arr[i]; 
+        }
+        else if(arr[i] > second_max && arr[i] != max){
+            second_max = arr[i];
+        }
+    }
+    
+    return second_max;
+}
