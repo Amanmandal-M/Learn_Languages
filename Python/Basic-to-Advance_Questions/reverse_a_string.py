8f1eeab7adbe5bf8ff3e76e8ee81473731b6d5f2
Amanmandal-M <amanmandal644@gmail.com>
2024-02-27 23:27:36 +0530

    Reverse a String

---
+++ Python/Basic-to-Advance_Questions/reverse_a_string.py
@@ -0,0 +1,9 @@
+# Problem Statement:
+# Write a Python function that takes a string as input and returns the reverse of that string.
+
+# Input:
+# A string s.
+
+# Output:
+# Return the reverse of the input string s.
+# For example, if the input string is "hello", the output should be "olleh".
