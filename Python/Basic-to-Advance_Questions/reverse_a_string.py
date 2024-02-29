cc3441c82c0d6d819f8bb6b080142ff413eb2d7d
Amanmandal-M <amanmandal644@gmail.com>
2024-02-29 23:46:35 +0530

    Reversed String Through two Methods

--- Python/Basic-to-Advance_Questions/reverse_a_string.py
+++ Python/Basic-to-Advance_Questions/reverse_a_string.py
@@ -9,7 +9,19 @@
 # For example, if the input string is "hello", the output should be "olleh".
 
 str = 'hello'
+
+### ---> 1st Method <---
+# Time Complexity (TC): O(n^2)
+# Space Complexity (SC): O(n)
+
 bag=""
 for i in range(len(str)-1,-1,-1):
     bag += str[i]
-print (bag)    
+print (bag)   
+
+### ---> 2nd Method <---
+# Time Complexity (TC): O(n)
+# Space Complexity (SC): O(n)
+
+reversed_str = str[::-1]
+print (reversed_str)
